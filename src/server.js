c93d182fc75a3af4b286b7612aab759ad296c93b
Bao Ly <bly3@charlotte.edu>
2026-04-19 15:30:24 -0400

    User and Auth

--- src/server.js
+++ src/server.js
@@ -1,1 +1,32 @@
+import express from 'express';
+import morgan from 'morgan';
+import authRoutes from './routes/authRoutes.js';
 
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+if (process.env.NODE_ENV !== 'test') app.use(morgan('tiny'));
+
+app.use('/api/auth', authRoutes);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    if (!err.status) {
+        err.status = 500;
+        err.message = 'Internal Server Error';
+    }
+    res.status(err.status).json({ error: err.message });
+});
+
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+}
+
+export default app;
